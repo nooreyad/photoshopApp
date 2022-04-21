452cea856d614deb7d66238d8cc356cff36ae7d2
NourMuhammad10 <nourmuhammad835@gmail.com>
2022-04-22 01:42:45 +0200

    Edits to blur function

--- main.cpp
+++ main.cpp
@@ -521,10 +521,8 @@ void BlurImage(){
     loadImage();
     for (int i = 0 ; i < SIZE ; i++){
         for (int j =0 ; j< SIZE ; j++){
-            long sum ;
             long double average ;
-            sum = image[i][j] + image[i-1][j-1] + image[i-1][j] + image [i-1][j+1] + image [i][j-1]+ image[i][j+1] + image [i+1][j-1] + image[i+1][j]+ image[i+1][j+1];
-            average = sum / 9;
+            average = (image[i][j] + image[i-1][j-1] + image[i-1][j] + image [i-1][j+1] + image [i][j-1]+ image[i][j+1] + image [i+1][j-1] + image[i+1][j]+ image[i+1][j+1])/9;
             image [i][j] = average;
         }
     }
